e20f4c2b69cdc6f92f3750789372da9342d055ab
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2022-05-19 18:55:26 +0200

    First Commit

--- app/src/androidTest/java/com/openclassrooms/magicgithub/UserListInstrumentedTest.java
+++ app/src/androidTest/java/com/openclassrooms/magicgithub/UserListInstrumentedTest.java
@@ -15,6 +15,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.openclassrooms.magicgithub.utils.RecyclerViewUtils.clickChildView;
+import com.openclassrooms.magicgithub.R;
 
 /**
  * Instrumented test, which will execute on an Android device.
